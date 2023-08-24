15a8d8d34745ddc9c394acb8b7a222022d8e1014
StphnX <136083929+StphnX@users.noreply.github.com>
2023-08-24 17:16:48 +0200

    amazing changes by Eca

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require("dotenv").config()
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const recipes = require("./recipes.json")
 const recipesRoute = require('./Routes/recipesRoutes');
 const newRecipeRoute = require('./Routes/newRecipeRoutes');
 const recipeRoute = require('./Routes/recipeRoute')
-
+const bodyParser = require('body-parser')
 
 
 
@@ -15,7 +16,8 @@ const port = process.env.PORT;
 
 
 
-
+app.use(cors());
+app.use(bodyParser.json())
 app.use(recipesRoute);
 app.use(newRecipeRoute);
 app.use(recipeRoute)
